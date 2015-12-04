fbd8d32d41eb9cca65a769f0ab849ac85f207696
hasulica <raduasandei@gmail.com>
2015-12-04 11:03:52 +0000

    Solved;

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,4 +1,21 @@
 class String
 
+  def to_wavecase
+    uppercase = false
+    new_string = ""
+    self.each_char do |i|
+      if i != ' '
+        if uppercase
+          new_string += i.upcase
+        else
+          new_string += i.downcase
+        end
+        uppercase = !uppercase
+      else
+        new_string += i
+      end
+    end
+    new_string
+  end
 
 end
